d38a39f31db2eab45a1ba6525f918788aedfc1b0
podral3 <podral3@gmail.com>
2025-10-23 22:40:37 +0200

    Add project files.

---
+++ CanvasSignalR/Hubs/CanvasHub.cs
@@ -0,0 +1,33 @@
+﻿using CanvasSignalR.Services;
+using Microsoft.AspNetCore.SignalR;
+using System.Collections.Concurrent;
+
+namespace CanvasSignalR.Hubs
+{
+    public class CanvasHub(CanvasService canvasService) : Hub
+    {
+        private readonly CanvasService _canvasService = canvasService;
+
+        public async Task JoinCanvas(string canvasName)
+        {
+            _canvasService.AddUserToCanvas(canvasName, Context.ConnectionId);
+            await Groups.AddToGroupAsync(Context.ConnectionId, canvasName);
+        }
+        public async Task LeaveCanvas(string canvasName)
+        {
+            _canvasService.RemoveUserFromCanvas(canvasName, Context.ConnectionId);
+            await Groups.RemoveFromGroupAsync(Context.ConnectionId, canvasName);
+        }
+
+        public async Task SendLine(string canvasName, int x1, int y1, int x2, int y2, int r, int g, int b, int penSize)
+        {
+            await Clients.OthersInGroup(canvasName).SendAsync("ReceiveCanvasUpdate", x1, y1, x2, y2, r, g, b, penSize);
+        }
+
+        public async Task<List<string>> GetCanvases()
+        {
+            return await Task.FromResult(_canvasService.GetCanvases());
+        }
+
+    }
+}
---
+++ CanvasSignalR/Services/CanvasService.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Concurrent;
+
+namespace CanvasSignalR.Services
+{
+    public class CanvasService
+    {
+        private readonly ConcurrentDictionary<string, HashSet<string>> _canvasUsers = new();
+        private readonly ConcurrentDictionary<string, string> _userByCanvas = new();
+
+        public void AddUserToCanvas(string canvasName, string connectionId) 
+        {
+            _canvasUsers.AddOrUpdate(
+               canvasName,
+               new HashSet<string> { connectionId },
+               (key, existing) => { existing.Add(connectionId); return existing; }
+           );
+
+            _userByCanvas.AddOrUpdate(
+                connectionId,
+                canvasName,
+                (key, existing) => canvasName);
+        }
+        public void RemoveUserFromCanvas(string canvasName, string connectionId)
+        {
+            if (_canvasUsers.TryGetValue(canvasName, out var users))
+            {
+                users.Remove(connectionId);
+                if(users.Count == 0) _canvasUsers.TryRemove(canvasName, out _);
+            }
+
+            _userByCanvas.TryRemove(connectionId, out _);
+        }
+        public List<string> GetCanvases() => [.. _canvasUsers.Keys];
+    }
+}
